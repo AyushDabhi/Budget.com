fa4bd7a530a23ada5cb107c990efac9b6ade4bef
AyushDabhi <76897206+AyushDabhi@users.noreply.github.com>
2022-06-17 22:24:49 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,10 @@
+function show() {
+    document.getElementById('option-tab').classList.toggle('active');
+}
+
+function conv(){
+
+    document. getElementById("cur_convert"). onclick(location. href = "Currency.html")
+          
+}
+
